0de134bc84ecd68e08a2ae3b276c4514c0f67f92
Kenan Kurt <kenantkurt@gmail.com>
2025-12-16 17:16:21 +0000

    update_finance_Days.sql

--- models/intermediate/int_sales_margin.sql
+++ models/intermediate/int_sales_margin.sql
@@ -12,14 +12,18 @@ products as (
 
 )
 
-select sales.date_date,
-sales.orders_id,
-sales.products_id,
-sales.revenue,
-sales.quantity,
-products.purchase_price,
-sales.quantity*products.purchase_price as purchase_cost,
-round(sales.revenue - (sales.quantity*products.purchase_price),2) as margin
+select
+    sales.date_date,
+    sales.orders_id,
+    sales.products_id as product_id,
+    sales.revenue,
+    sales.quantity,
+    products.purchase_price,
+    sales.quantity * products.purchase_price as purchase_cost,
+    round(
+        sales.revenue - (sales.quantity * products.purchase_price),
+        2
+    ) as margin
 from sales
 left join products
-    on sales.products_id = products.products_id 
+    on sales.products_id = products.products_id
--- models/mart/finance_days.sql
+++ models/mart/finance_days.sql
@@ -16,13 +16,13 @@ ship as (
 
 select orders_margin.date,
 count(orders_margin.orders_id) as number_of_transaction,
-sum(orders_margin.total_revenue) as total_revenue,
-sum(orders_margin.total_revenue)/count(orders_margin.orders_id) as average_basket,
-sum(orders_operational.operational_margin) as operational_margin,
-sum(orders_margin.total_purchase_cost) as total_purchase_cost,
-sum(ship.shipping_fee) as total_shipping_fees,
-sum(ship.logcost) as total_log_costs,
-sum(orders_margin.nb_of_products) as total_nb_of_products
+round(sum(orders_margin.total_revenue),2) as total_revenue,
+round(sum(orders_margin.total_revenue)/count(orders_margin.orders_id),2) as average_basket,
+round(sum(orders_operational.operational_margin),2) as operational_margin,
+round(sum(orders_margin.total_purchase_cost),2) as total_purchase_cost,
+round(sum(ship.shipping_fee),2) as total_shipping_fees,
+round(sum(ship.logcost),2) as total_log_costs,
+round(sum(orders_margin.nb_of_products),2) as total_nb_of_products
 from orders_margin left join orders_operational on orders_operational.orders_id = orders_margin.orders_id
 left join ship on ship.orders_id = orders_margin.orders_id
 group by orders_margin.date
